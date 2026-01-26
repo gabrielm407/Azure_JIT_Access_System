96a6b8a0378d475f9368fd5e3ea28369364ee7f4
gabrielm407 <moffettgabriel@gmail.com>
2026-01-25 17:26:32 -0800

    roles and provision

---
+++ managed_identity.tf
@@ -0,0 +1,11 @@
+resource "azurerm_user_assigned_identity" "user" {
+  name                = "user-managed-identity"
+  location            = module.resource_group[local.default_environment].location
+  resource_group_name = module.resource_group[local.default_environment].name
+}
+
+resource "azurerm_role_assignment" "storage_blob_data_contributor" {
+  scope                = module.resource_group[local.default_environment].resource_group_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_user_assigned_identity.user.principal_id
+}
