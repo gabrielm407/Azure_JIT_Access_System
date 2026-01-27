b531bcb02a1053f747412b1f9cd28d87d3489891
gabrielm407 <moffettgabriel@gmail.com>
2026-01-26 17:47:05 -0800

    Wrap classes in namespace to fix collision

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 
+// Add this if you reference the classes explicitly, otherwise standard code is fine.
+using JitAccessConfig; 
+
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
